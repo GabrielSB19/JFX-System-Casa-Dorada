cf844dfc26cab5a74db23181d59134b300389820
Gabriel Suarez <gabriel.suarez1@correo.icesi.edu.co>
2021-03-16 09:16:03 -0500

    Function of the buttons

--- src/ui/FXControllerGUI.java
+++ src/ui/FXControllerGUI.java
@@ -18,11 +18,14 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import model.*;
 
 public class FXControllerGUI implements Initializable {
+    
+    //SplashScreen
 
     public static ImageView imageView;
 
@@ -31,32 +34,42 @@ public class FXControllerGUI implements Initializable {
     public static Timeline timeline;
 
     @FXML
-    private BorderPane bpMain;
-
-    @FXML
     public JFXSpinner jfxSpinner;
 
-    @FXML
-    private ImageView ivLoginP;
 
     @FXML
     private ImageView ivWelcome;
     
+    //Login
+    
     @FXML
-    private ImageView ivGoldHouse;
-    /*
-    @FXML
-    private ImageView ivTest;
-    */
-
-    @FXML
-    private BorderPane bpRegister;
-
+    private BorderPane bpMain;
+    
     @FXML
     private JFXTextField txtUserLogin;
 
     @FXML
     private JFXPasswordField txtPassWordLogin;
+    
+    //Register
+    
+    @FXML
+    private BorderPane bpRegister;
+    
+    //Menu
+    
+    @FXML
+    private ImageView ivGoldHouse;
+    
+    @FXML
+    private BorderPane bpMenu;
+    
+    @FXML
+    private Pane pNewOption;
+    /*
+    @FXML
+    private ImageView ivTest;
+    */
 
     public FXControllerGUI(CasaDorada casaDorada) {
         this.casaDorada = casaDorada;
@@ -85,57 +98,8 @@ public class FXControllerGUI implements Initializable {
             FXMain.loaded = true;
         }
     }
-
     
-    @FXML
-    public void onCreateAccount(ActionEvent event) throws IOException {
-        closeStage();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/Register.fxml"));
-        fxmlLoader.setController(this);
-        Parent root = fxmlLoader.load();
-        newStage(root);
-    }
-
-    @FXML
-    public void onLogIn(ActionEvent event) throws IOException {
-        closeStage();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/Menu.fxml"));
-        fxmlLoader.setController(this);
-        Parent root = fxmlLoader.load();
-        ivGoldHouse.setImage(new Image("image/CasaDoradaNew.png"));
-        newStage(root);
-    }
-
-    @FXML
-    public void onIhaveAccount(ActionEvent event) throws IOException {
-        closeStage();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/Login.fxml"));
-        fxmlLoader.setController(this);
-        Parent root = fxmlLoader.load();
-        newStage(root);
-    }
     
-    @FXML
-    void onRegister(ActionEvent event) {
-    }
-
-    public void setImageWelcome() {
-        try {
-            ivWelcome.setImage(new Image("image/CasaDoradaNew.png"));
-        } catch (Exception e) {
-        }
-    }
-    
-    /*
-    public void setImageBanner(){
-        try{
-            ivTest.setImage(new Image("image/BannerCasaDoradaNew.png"));
-        } catch (Exception e){
-            
-        }
-    }
-    */
-
     public void changeScreen() {
         try {
             closeStage();
@@ -160,4 +124,209 @@ public class FXControllerGUI implements Initializable {
         Stage stage = (Stage) bpMain.getScene().getWindow();
         stage.close();
     }
+    
+    public void setImageWelcome() {
+        try {
+            ivWelcome.setImage(new Image("image/CasaDoradaNew.png"));
+        } catch (Exception e) {
+        }
+    }
+    
+    //Login
+    
+    @FXML
+    public void onCreateAccount(ActionEvent event) throws IOException {
+        closeStage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/Register.fxml"));
+        fxmlLoader.setController(this);
+        Parent root = fxmlLoader.load();
+        newStage(root);
+    }
+
+    @FXML
+    public void onLogIn(ActionEvent event) throws IOException {
+        closeStage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/Menu.fxml"));
+        fxmlLoader.setController(this);
+        Parent root = fxmlLoader.load();
+        ivGoldHouse.setImage(new Image("image/CasaDoradaNew.png"));
+        newStage(root);
+    }
+    
+    //Register
+
+    @FXML
+    public void onIhaveAccount(ActionEvent event) throws IOException {
+        closeStage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/Login.fxml"));
+        fxmlLoader.setController(this);
+        Parent root = fxmlLoader.load();
+        newStage(root);
+    }
+    
+    @FXML
+    public void onRegister(ActionEvent event) throws IOException {
+        closeStage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/Menu.fxml"));
+        fxmlLoader.setController(this);
+        Parent root = fxmlLoader.load();
+        ivGoldHouse.setImage(new Image("image/CasaDoradaNew.png"));
+        newStage(root);
+    }
+    
+    //Menu
+    
+    //Salir
+    
+    @FXML
+    public void onExitProgram(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    public void onLogOut(ActionEvent event) throws IOException {
+        Stage stage = (Stage) bpMenu.getScene().getWindow();
+        stage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/Login.fxml"));
+        fxmlLoader.setController(this);
+        Parent root = fxmlLoader.load();
+        newStage(root);
+    }
+    
+    //Gestion
+    
+    @FXML
+    public void onGestionUser(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/UsernameGestion.fxml"));
+    	
+    	fxmlLoader.setController(this);
+    	Parent usernameGestion = fxmlLoader.load();
+    	
+    	pNewOption.getChildren().clear();
+    	pNewOption.getChildren().setAll(usernameGestion);
+    }
+    
+    @FXML
+    public void onGestionClients(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/ClientsGestion.fxml"));
+    	
+    	fxmlLoader.setController(this);
+    	Parent clientsGestion = fxmlLoader.load();
+    	
+    	pNewOption.getChildren().clear();
+    	pNewOption.getChildren().setAll(clientsGestion);
+    }
+
+    @FXML
+    public void onGestionEmployee(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/EmployeesGestion.fxml"));
+    	
+    	fxmlLoader.setController(this);
+    	Parent employeesGestion = fxmlLoader.load();
+    	
+    	pNewOption.getChildren().clear();
+    	pNewOption.getChildren().setAll(employeesGestion);
+    }
+
+    @FXML
+    public void onGestionIngredients(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/IngredientsGestion.fxml"));
+    	
+    	fxmlLoader.setController(this);
+    	Parent ingredientsGestion = fxmlLoader.load();
+    	
+    	pNewOption.getChildren().clear();
+    	pNewOption.getChildren().setAll(ingredientsGestion);
+    }
+
+    @FXML
+    public void onGestionProducts(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/ProductsGestion.fxml"));
+    	
+    	fxmlLoader.setController(this);
+    	Parent productsGestion = fxmlLoader.load();
+    	
+    	pNewOption.getChildren().clear();
+    	pNewOption.getChildren().setAll(productsGestion);
+    }
+
+    @FXML
+    public void onGestionTypeProduct(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/TypeProductsGestion.fxml"));
+    	
+    	fxmlLoader.setController(this);
+    	Parent typeProductsGestion = fxmlLoader.load();
+    	
+    	pNewOption.getChildren().clear();
+    	pNewOption.getChildren().setAll(typeProductsGestion);
+    }
+    
+    //Listas
+    
+    @FXML
+    public void onLIstClients(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/ListClients.fxml"));
+    	
+    	fxmlLoader.setController(this);
+    	Parent listClients = fxmlLoader.load();
+    	
+    	pNewOption.getChildren().clear();
+    	pNewOption.getChildren().setAll(listClients);
+    }
+    
+    @FXML
+    public void onLIstEmployees(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/ListEmployees.fxml"));
+    	
+    	fxmlLoader.setController(this);
+    	Parent listEmployees = fxmlLoader.load();
+    	
+    	pNewOption.getChildren().clear();
+    	pNewOption.getChildren().setAll(listEmployees);
+    }
+
+    @FXML
+    public void onLIstIngredients(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/ListIngredients.fxml"));
+    	
+    	fxmlLoader.setController(this);
+    	Parent listIngredients = fxmlLoader.load();
+    	
+    	pNewOption.getChildren().clear();
+    	pNewOption.getChildren().setAll(listIngredients);
+    }
+
+    @FXML
+    public void onLIstProducts(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/ListProducts.fxml"));
+    	
+    	fxmlLoader.setController(this);
+    	Parent listProducts= fxmlLoader.load();
+    	
+    	pNewOption.getChildren().clear();
+    	pNewOption.getChildren().setAll(listProducts);
+    }
+
+    @FXML
+    void onLIstTypeProduct(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/ListTypeProducts.fxml"));
+    	
+    	fxmlLoader.setController(this);
+    	Parent listTypeProducts = fxmlLoader.load();
+    	
+    	pNewOption.getChildren().clear();
+    	pNewOption.getChildren().setAll(listTypeProducts);
+    }
+
+    
+    /*
+    public void setImageBanner(){
+        try{
+            ivTest.setImage(new Image("image/BannerCasaDoradaNew.png"));
+        } catch (Exception e){
+            
+        }
+    }
+    */
+
 }
